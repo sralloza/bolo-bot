ffed1cce8f2e987f8615666be762f581c2375a13
sralloza <sralloza@gmail.com>
2021-02-09 23:15:05 +0100

    refactor: sort imports

--- app/api/routes/info.py
+++ app/api/routes/info.py
@@ -1,8 +1,10 @@
-from app.schemas.info import SuccessWebHookInfo
-from app.core.info import get_webhook_info
 from fastapi.routing import APIRouter
 
+from app.core.info import get_webhook_info
+from app.schemas.info import SuccessWebHookInfo
+
 router = APIRouter(tags=["info"])
+
 
 @router.get("/webhook-info", response_model=SuccessWebHookInfo)
 def webhook_info_endpoint():
