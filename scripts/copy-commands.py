e8a62027a4c731b738b64811a632dfcf9e57c529
sralloza <sralloza@gmail.com>
2021-02-16 15:56:44 +0100

    feat: add command in copy-commands script to upload scripts via API

--- scripts/copy-commands.py
+++ scripts/copy-commands.py
@@ -1,7 +1,11 @@
 import re
 from pathlib import Path
+from typing import List
 
 import typer
+from telegram import BotCommand
+
+from app.core.bot import create_updater
 
 try:
     import pyperclip
@@ -9,22 +13,38 @@ except ImportError:
     typer.secho("You need to install pyperclip", fg="bright_red")
     raise typer.Abort()
 
-app = typer.Typer(add_completion=False)
+app = typer.Typer(add_completion=False, help="Bot's commands manager")
 
 
-@app.command()
-def copy_commands():
+def get_commands() -> List[BotCommand]:
     commands_path = Path(__file__).parent.with_name("commands.md")
     commands_info = commands_path.read_text("utf8")
 
-    filtered = []
+    commands = []
     for line in commands_info.splitlines():
         if line.strip().startswith("-"):
             line = re.sub(r"- \/([\w]+)", r"\1", line).replace("\\_", "_")
-            filtered.append(line)
+            command, description = re.split(" - ", line, maxsplit=1)
+            commands.append(BotCommand(command, description))
 
-    result = "\n".join(filtered)
+    return commands
+
+
+@app.command("copy")
+def copy_commands():
+    """Copies the command in a format @BotFather understands."""
+    commands = get_commands()
+    result = "\n".join([f"{x.command} - {x.description}" for x in commands])
     pyperclip.copy(result)
+    typer.secho("Done", fg="bright_green")
+
+
+@app.command("update")
+def update_commands():
+    """Updates the bot's commands via Telegram API."""
+    bot = create_updater().bot
+    commands = get_commands()
+    bot.set_my_commands(commands)
     typer.secho("Done", fg="bright_green")
 
 
